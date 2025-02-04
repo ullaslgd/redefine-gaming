9c31a4c1bead23ed96b11f93b736ddacf3a3e30e
Ullas <clubullas@gmail.com>
2025-02-04 23:37:49 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import NavBar from './components/NavBar'
+import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
         <NavBar />
         <Hero />
         <About />
+        <Features />
+        <Story />
+        <Contact />
+        <Footer />
    </main>
   )
 }
